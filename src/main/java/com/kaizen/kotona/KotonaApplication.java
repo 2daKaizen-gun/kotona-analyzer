178036f5cc652097ac8c0de4aa6e6ebdb912b499
2daKaizen-gun <hkys1223@gmail.com>
2026-02-20 12:52:51 +0900

    feat: enable Datasource and integrate Dotenv for environment variables

--- src/main/java/com/kaizen/kotona/KotonaApplication.java
+++ src/main/java/com/kaizen/kotona/KotonaApplication.java
@@ -1,13 +1,23 @@
 package com.kaizen.kotona;
 
+import io.github.cdimascio.dotenv.Dotenv; // Dotenv 임포트
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 // exclude setting
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class KotonaApplication {
     public static void main(String[] args) {
+        // 스프링 실행 전 .env 파일 읽어서 시스템 변수 등록
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        // 스프링 부트 실행
         SpringApplication.run(KotonaApplication.class, args);
     }
 }
