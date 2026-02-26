acea7059e595d374131623609e3d4245b2c2077b
2daKaizen-gun <hkys1223@gmail.com>
2026-02-27 01:18:59 +0900

    feat: implement hybrid scoring logic 1

---
+++ src/main/java/com/kaizen/kotona/service/AnalysisValidator.java
@@ -0,0 +1,12 @@
+package com.kaizen.kotona.service;
+
+import com.kaizen.kotona.dto.NuanceResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class AnalysisValidator {
+}
