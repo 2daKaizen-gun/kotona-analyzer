579f3a10d62290636e8edeb833b94f4fb212c357
2daKaizen-gun <hkys1223@gmail.com>
2026-02-28 14:01:55 +0900

    feat: integrating morphological analysis and hybrid validation into GeminiService

--- src/main/java/com/kaizen/kotona/service/AnalysisValidator.java
+++ src/main/java/com/kaizen/kotona/service/AnalysisValidator.java
@@ -4,8 +4,6 @@ import com.kaizen.kotona.dto.NuanceResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Slf4j
 @Service
 public class AnalysisValidator {
