9336166f7b5d730c23f13523dee0367e701e364a
2daKaizen-gun <hkys1223@gmail.com>
2026-03-01 11:15:42 +0900

    feat: upgrade DTO to 100-point scale with detailed metrics 3

--- src/main/java/com/kaizen/kotona/dto/MetricsDTO.java
+++ src/main/java/com/kaizen/kotona/dto/MetricsDTO.java
@@ -1,7 +1,7 @@
 package com.kaizen.kotona.dto;
 
-public record MetricsDTO {
+public record MetricsDTO (
     int politeness, // 경어 사용(0~40)
     int indirectness, // 간접 화법(0~40)
     int etiquette // 쿠션어, 에티켓(0~30)
-} {}
+) {}
