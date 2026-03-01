1e9c83be18ef50df46aa8d60f73fc5c607ac54ad
2daKaizen-gun <hkys1223@gmail.com>
2026-03-01 11:10:15 +0900

    feat: upgrade DTO to 100-point scale with detailed metrics 1

--- src/main/java/com/kaizen/kotona/dto/NuanceResponseDTO.java
+++ src/main/java/com/kaizen/kotona/dto/NuanceResponseDTO.java
@@ -1,9 +1,13 @@
 package com.kaizen.kotona.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record NuanceResponseDTO(
-    int score,
+    @JsonProperty("totalScore")
+    int totalScore, // 0 ~ 100
+
+    MetricsDTO metrics,
     EvaluationDTO evaluation,
     FeedbackDTO feedback,
     List<SuggestionDTO> suggestions
