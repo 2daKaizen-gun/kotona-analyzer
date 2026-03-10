c72642b1e48c95c1f81e0f7af5c841f53821c56f
2daKaizen-gun <hkys1223@gmail.com>
2026-03-10 09:37:48 +0900

    config: set Swagger/OpenAPI configuration with metadata 4

--- src/main/java/com/kaizen/kotona/config/OpenApiConfig.java
+++ src/main/java/com/kaizen/kotona/config/OpenApiConfig.java
@@ -12,7 +12,8 @@ public class OpenApiConfig {
     public OpenAPI kotonaOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-
-                );
+                        .title("KOTONA API Documentation")
+                        .description("일본 비즈니스 메일 분석 및 숙어 사전 서비스 API")
+                        .version("v1.0.0"));
     }
 }
