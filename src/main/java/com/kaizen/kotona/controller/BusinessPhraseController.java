fa5183e491f8529d1aadd937d22f6c83fd4968de
2daKaizen-gun <hkys1223@gmail.com>
2026-03-10 09:40:26 +0900

    docs: add OpenAPI annotations to BusinessPhraseController

--- src/main/java/com/kaizen/kotona/controller/BusinessPhraseController.java
+++ src/main/java/com/kaizen/kotona/controller/BusinessPhraseController.java
@@ -2,6 +2,8 @@ package com.kaizen.kotona.controller;
 
 import com.kaizen.kotona.entity.BusinessPhrase;
 import com.kaizen.kotona.service.BusinessPhraseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,11 +12,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/phrases")
 @RequiredArgsConstructor
+// Swagger 그룹화
+@Tag(name = "Business Phrase", description = "비즈니스 일본어 숙어 라이브러리 API")
 public class BusinessPhraseController {
 
     private final BusinessPhraseService service;
 
     // 모든 숙어 목록 조회 (GET http://localhost:8081/api/phrases)
+    @Operation(summary = "모든 숙어 조회", description = "DB에 저장된 모든 비즈니스 숙어를 정중도 순으로 조회")
     @GetMapping
     public List<BusinessPhrase> getAllPhrases() {
         return service.getAllPhrases();
