24fc39d237963d91f6c2d0f9d4883402f53af1c1
2daKaizen-gun <hkys1223@gmail.com>
2026-03-10 09:36:07 +0900

    config: set Swagger/OpenAPI configuration with metadata 2

--- src/main/java/com/kaizen/kotona/config/OpenApiConfig.java
+++ src/main/java/com/kaizen/kotona/config/OpenApiConfig.java
@@ -1,4 +1,15 @@
 package com.kaizen.kotona.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI kotonaOpenAPI() {
+        return new OpenAPI()
+
+    }
 }
