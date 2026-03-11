798fd747286a44285451812a17362d25d1ef3614
2daKaizen-gun <hkys1223@gmail.com>
2026-03-11 11:24:31 +0900

    test: implement unit tests for BusinessPhraseService using Mockito 2

--- src/test/java/com/kotona/analyzer/BusinessPhraseServiceTest.java
+++ src/test/java/com/kotona/analyzer/BusinessPhraseServiceTest.java
@@ -1,4 +1,10 @@
 package com.kotona.analyzer;
 
-public class BusinessPhraseServiceTest {
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+// Mockito 사용 설정
+@ExtendWith(MockitoExtension.class)
+class BusinessPhraseServiceTest {
+
 }
