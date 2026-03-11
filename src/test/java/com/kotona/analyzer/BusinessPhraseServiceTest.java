dd2c9b63484dfef1c55c2475640ca9272982e0d4
2daKaizen-gun <hkys1223@gmail.com>
2026-03-11 11:27:32 +0900

    test: implement unit tests for BusinessPhraseService using Mockito 3

--- src/test/java/com/kotona/analyzer/BusinessPhraseServiceTest.java
+++ src/test/java/com/kotona/analyzer/BusinessPhraseServiceTest.java
@@ -1,10 +1,29 @@
 package com.kotona.analyzer;
 
+import com.kaizen.kotona.repository.BusinessPhraseRepository;
+import com.kaizen.kotona.service.BusinessPhraseService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 // Mockito 사용 설정
 @ExtendWith(MockitoExtension.class)
 class BusinessPhraseServiceTest {
 
+    @Mock
+    // 가짜 객체 생성
+    private BusinessPhraseRepository repository;
+
+    @InjectMocks
+    // Mock 객체를 서비스에 주입
+    private BusinessPhraseService service;
+
+    @Test
+    @DisplayName("전체 숙어 조회 시 정중도 내림차순으로 반환되어야 함.")
+    void getAllPhrasesTest() {
+
+    }
 }
