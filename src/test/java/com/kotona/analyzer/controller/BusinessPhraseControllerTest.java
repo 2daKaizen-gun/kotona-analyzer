0a0f2e18b266d94a1f0234c0c387ebdfb3d515fb
2daKaizen-gun <hkys1223@gmail.com>
2026-03-11 11:47:47 +0900

    test: add MockMvc tests for BusinessPhraseController endpoints 3

--- src/test/java/com/kotona/analyzer/controller/BusinessPhraseControllerTest.java
+++ src/test/java/com/kotona/analyzer/controller/BusinessPhraseControllerTest.java
@@ -1,8 +1,10 @@
 package com.kotona.analyzer.controller;
 
 import com.kaizen.kotona.controller.BusinessPhraseController;
+import com.kaizen.kotona.service.BusinessPhraseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(BusinessPhraseController.class)
@@ -11,4 +13,10 @@ class BusinessPhraseControllerTest {
     @Autowired
     // API 호출 시뮬레이션
     private MockMvc mockMvc;
+
+    @MockitoBean
+    // 컨트롤러가 의존하는 서비스를 Mock으로 대체
+    private BusinessPhraseService service;
+
+
 }
