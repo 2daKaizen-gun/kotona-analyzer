21141fb237d5f1369f484e5d4227371fefd7c046
2daKaizen-gun <hkys1223@gmail.com>
2026-03-11 11:32:54 +0900

    test: implement unit tests for BusinessPhraseService using Mockito 4

--- src/test/java/com/kotona/analyzer/BusinessPhraseServiceTest.java
+++ src/test/java/com/kotona/analyzer/BusinessPhraseServiceTest.java
@@ -1,5 +1,8 @@
 package com.kotona.analyzer;
 
+import static org.mockito.BDDMockito.*;
+
+import com.kaizen.kotona.entity.BusinessPhrase;
 import com.kaizen.kotona.repository.BusinessPhraseRepository;
 import com.kaizen.kotona.service.BusinessPhraseService;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 // Mockito 사용 설정
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +29,12 @@ class BusinessPhraseServiceTest {
     @Test
     @DisplayName("전체 숙어 조회 시 정중도 내림차순으로 반환되어야 함.")
     void getAllPhrasesTest() {
+        // given: 가짜 데이터와 행동 정의함
+        BusinessPhrase phrase1 = new BusinessPhrase(1L, "承知いたしました", "알겠습니다", "EMAIL", 5, "예시1");
+        BusinessPhrase phrase2 = new BusinessPhrase(2L, "念のため", "만약을 위해", "CONFIRMATION", 2, "예시2");
+        given(repository.findAllByOrderByPolitenessLevelDesc()).willReturn(List.of(phrase1, phrase2));
 
+        // 테스트할 메서드 실행
+        List<BusinessPhrase> result = service.getAllPhrases();
     }
 }
